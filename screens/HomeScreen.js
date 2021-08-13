80165f5a3069b7170296f16cef589424e4e52c11
marlon felipe martins de oliveira <marlon@pop-os.localdomain>
2021-08-12 23:32:37 -0300

    iniciando

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Button, View, Text, TextInput } from 'react-native';
+import { Button, View, Text, TextInput, TouchableOpacity, ViewBase } from 'react-native';
 import React, { Component } from "react";
 import { styles } from "../style/Home";
 
@@ -29,15 +29,29 @@ export default class HomeScreen extends Component {
         
         return (
             <View style={styles.container}>
-                <View>
+                <View style={styles.containerLoginRegister}>
+                    <View style={styles.login}>
+                        <TouchableOpacity>
+                            <Text>Login</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.register}>
+                        <TouchableOpacity>
+                            <Text>Register</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+                <View style={styles.containerInput}>
                     <TextInput
+                        style={styles.input}
                         placeholder="Pesquise a resposta para qualquer pergunta..."
                     />
-
                 </View>
-                <View>
-                    <Text>Qual sua dúvida?</Text>
+                <View style={styles.containerInput}>
+                    <Text style={styles.text1}>Qual sua dúvida?</Text>
                     <Button
+                        style={styles.btn}
                         title="faça sua pergunta"
                     />
                 </View>
--- style/Home.js
+++ style/Home.js
@@ -4,8 +4,41 @@ export const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      //alignItems: 'center',
+      //justifyContent: 'center',
     },
+    containerInput: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input:{
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 10,
+        marginTop: 10,
+    },
+    text1:{
+        color: 'black',
+        textAlign: "center",
+        fontSize: 35,
+        marginTop: 10,
+        fontWeight: "bold"
+    },
+    btn:{
+        color: '#2196F3'
+    },
+    containerLoginRegister:{
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    login:{
+        //backgroundColor: 'red',
+        marginRight: 4
+    },
+    register:{
+        //backgroundColor: 'blue'
+        marginRight: 4
+    }
     
 });
